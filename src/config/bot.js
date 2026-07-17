2040a34e7829ac171343b4d502645e35ac341ab8
xenom4t1c-1 <xenom4t1c@gmail.com>
2026-07-17 13:06:00 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "SERVER IP: 51.79.249.13:7826 (BETA TEST)",
+        name: "51.79.249.13:7826 (BETA TEST)",
         // Activity type number (0 = Playing).
         type: 0,
       },
