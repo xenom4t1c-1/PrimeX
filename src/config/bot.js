df2a2ed977d5e038a1358ee4efe3df5d3978040d
xenom4t1c-1 <xenom4t1c@gmail.com>
2026-07-24 21:05:08 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "51.79.249.13:7826 (BETA TEST)",
+        name: "samp.pxr-rp.site:7826 (BETA TEST)",
         // Activity type number (0 = Playing).
         type: 0,
       },
